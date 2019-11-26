c22f3dc9cd67054565f14148f9e2ca6584b5c42e
Phoenix Ooi <cf.ooi96@gmail.com>
2019-11-26 11:54:52 +0800

    make it better

---
+++ client/build/precache-manifest.3f9d01ef95b63ae2b593f0e16231ffd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e9e7b3eb8e87c72b9b7d6267978fa85",
+    "url": "/static/media/news.0e9e7b3e.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "abe740b06a97443c599a",
+    "url": "/static/js/main.abe740b0.chunk.js"
+  },
+  {
+    "revision": "534f457c4e233b7b6fb5",
+    "url": "/static/js/1.534f457c.chunk.js"
+  },
+  {
+    "revision": "abe740b06a97443c599a",
+    "url": "/static/css/main.ae6f470d.chunk.css"
+  },
+  {
+    "revision": "534f457c4e233b7b6fb5",
+    "url": "/static/css/1.5daea31a.chunk.css"
+  },
+  {
+    "revision": "b6c65d52c9a8cc6018a8ed514442da8b",
+    "url": "/index.html"
+  }
+];
